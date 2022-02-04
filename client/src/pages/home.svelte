60bde708d60577f76468b0099bb33dcda4a7b599
Xavier <xmtrinidad@gmail.com>
2022-02-03 20:36:20 -0700

    testing basic click event

--- client/src/pages/home.svelte
+++ client/src/pages/home.svelte
@@ -1,9 +1,17 @@
 <script>
   console.log('home page loaded');
+
+  function createWutDo() {
+    console.log('create wut do works');
+  }
+
 </script>
 
 <div class="home-page">
   <h1>WUT DO - WITH TROLL FONT (NOT ROBOTO)</h1>
+
+  <!-- Learning click events -->
+  <button on:click="{createWutDo}" class="add-btn">Add WUT DO</button>
 
   <!-- Component ideas -->
   <!-- NOT sure if to make this like a dashboard page, then you can click a thing to view previous lists.  Or maybe just one big list of things.  IDK, what you think @Matt Kilcup -->
@@ -23,6 +31,15 @@
 
   h1 {
     font-size: 2rem;
+  }
+
+  .add-btn {
+    padding: 12px;
+    margin-top: 12px;
+    background-color: #673AB7;
+    border-radius: 4px;
+    color: #fff;
+    box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);
   }
 </style>
 
