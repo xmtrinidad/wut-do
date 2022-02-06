7e957f2f8d2995785926236a07505ac4cd45da32
Xavier <xmtrinidad@gmail.com>
2022-02-06 09:57:52 -0700

    added demo route

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,8 +1,12 @@
 <script>
   // import Router, { location, link } from 'svelte-spa-router';
   // import { wrap } from 'svelte-spa-router/wrap';
+
+  // Dynamic import these eventually when we learn the ways
   import Login from './pages/login.svelte';
   import Home from './pages/home.svelte';
+  import Demo from './pages/demo.svelte';
+
   import AppHeader from './components/app-header.svelte';
   import { Router, Link, Route } from 'svelte-routing';
   export let url = '';
@@ -19,6 +23,7 @@
     </nav>
     <div>
       <Route path="login" component={Login} />
+      <Route path="/demo" component={Demo} />
       <Route path="/" component={Home} />
     </div>
   </Router>
---
+++ client/src/pages/demo.svelte
@@ -0,0 +1,9 @@
+<script>
+  console.log('demo page loaded');
+</script>
+
+<p>This is a demo page to learn the ways of Svelte</p>
+
+<style>
+
+</style>
