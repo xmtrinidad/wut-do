40d49c669fbd975d26233ef3e053bbaeb42aa1fa
Xavier <xmtrinidad@gmail.com>
2022-02-06 11:12:00 -0700

    added attributes plus click demo

---
+++ client/src/components/demos/attributes-demo.svelte
@@ -0,0 +1,39 @@
+<script>
+  const gifs = [
+    'https://media2.giphy.com/media/MYN98N5ZNGoA3lJ6yT/giphy.gif',
+    'https://media1.giphy.com/media/SANfwj5dMkjWpWyBGK/giphy.gif',
+    'https://media3.giphy.com/media/UUgHwIBcib16awR00q/giphy.gif?cid=790b761128184aa57a1b95ba4345fd4b37b8435fb40a0774&rid=giphy.gif&ct=g'
+  ];
+  let index = 0;
+	let src = gifs[index];
+
+  function onNewGifClick() {
+    index += 1;
+    if (!gifs[index]) {
+      index = 0;
+    }
+
+    src = gifs[index]
+  }
+</script>
+
+<!-- {src} is short for src={src} -->
+<div class="attributes-demo">
+  <h1>Click + Attribute Change</h1>
+  <div class="gif-container">
+    <img {src} alt="">
+    <button class="new-gif-btn" on:click="{onNewGifClick}">NEW GIF</button>
+  </div>
+</div>
+
+<style>
+  h1 {
+    font-size: 2rem;
+    font-weight: bold;
+  }
+
+  .gif-container {
+    margin-top: 24px;
+  }
+</style>
+
--- client/src/pages/demo.svelte
+++ client/src/pages/demo.svelte
@@ -1,9 +1,16 @@
 <script>
+import AttributesDemo from "../components/demos/attributes-demo.svelte";
+
   console.log('demo page loaded');
 </script>
 
-<p>This is a demo page to learn the ways of Svelte</p>
+<!-- This is a demo page to learn the ways of Svelte -->
+<div class="demos">
+  <AttributesDemo></AttributesDemo>
+</div>
 
 <style>
-
+  .demos {
+    padding: 24px;
+  }
 </style>
