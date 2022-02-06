01273396884cead5885503027978caeecbd89ef7
Xavier <xmtrinidad@gmail.com>
2022-02-06 11:51:27 -0700

    added dynamic html rendering example

---
+++ client/src/components/demos/html-render-demo.svelte
@@ -0,0 +1,33 @@
+<script>
+  const stonks = true;
+  let stonksHTML = stonks ? `
+    <div>
+      <h1>STONKS</h1>
+      <img src="https://wompampsupport.azureedge.net/fetchimage?siteId=7575&v=2&jpgQuality=100&width=700&url=https%3A%2F%2Fi.kym-cdn.com%2Fentries%2Ficons%2Foriginal%2F000%2F029%2F959%2FScreen_Shot_2019-06-05_at_1.26.32_PM.jpg" alt="">
+    </div>
+  ` : `
+    <div>
+      <h1>NOT STONKS</h1>
+      <img src="https://i.kym-cdn.com/photos/images/facebook/001/779/895/752.jpg" alt="">
+    </div>
+  `;
+</script>
+
+<!-- {src} is short for src={src} -->
+<div class="html-render-demo">
+  {@html stonksHTML}
+</div>
+
+<style>
+  h1 {
+    
+  }
+
+  /* Applying styles to string HTML doesn't work without global, this is the apparent solution https://stackoverflow.com/questions/59669571/svelte-dynamic-html-elements-do-not-have-css-applied */
+  .html-render-demo :global(h1) {
+    font-size: 2rem;
+    font-weight: bold;
+    border: 2px solid red;
+  }
+</style>
+
--- client/src/pages/demo.svelte
+++ client/src/pages/demo.svelte
@@ -1,16 +1,26 @@
 <script>
 import AttributesDemo from "../components/demos/attributes-demo.svelte";
+import HtmlRenderDemo from '../components/demos/html-render-demo.svelte';
 
   console.log('demo page loaded');
 </script>
 
 <!-- This is a demo page to learn the ways of Svelte -->
 <div class="demos">
-  <AttributesDemo></AttributesDemo>
+  <div class="demo">
+    <AttributesDemo></AttributesDemo>
+  </div>
+  <div class="demo">
+    <HtmlRenderDemo></HtmlRenderDemo>
+  </div>
 </div>
 
 <style>
   .demos {
     padding: 24px;
+  }
+
+  .demo {
+    margin-bottom: 24px;
   }
 </style>
