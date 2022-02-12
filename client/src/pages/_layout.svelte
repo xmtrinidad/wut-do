3e00e1f6ab544f6c7acf34c888710d71724df2ea
Matt <nevermindthelabel@gmail.com>
2022-02-11 21:40:32 -0700

    added more stuff

---
+++ client/src/pages/_layout.svelte
@@ -0,0 +1,18 @@
+<script>
+  import AppHeader from '../components/app-header.svelte';
+  import AppFooter from '../components/app-footer.svelte';
+</script>
+
+<main>
+  <AppHeader />
+  <slot />
+  <AppFooter />
+</main>
+
+<style>
+  main {
+    display: grid;
+    grid-template-rows: auto 1fr auto;
+    min-height: 100vh;
+  }
+</style>
