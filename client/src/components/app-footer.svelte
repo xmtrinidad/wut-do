cdd5671916588afee3c6da51392f363eb869a7b0
Matt <nevermindthelabel@gmail.com>
2022-02-11 21:37:05 -0700

    added footer

---
+++ client/src/components/app-footer.svelte
@@ -0,0 +1,18 @@
+<script>
+  console.log('we have a footer');
+  export const date = new Date().getFullYear();
+</script>
+
+<footer>
+  @copy {date}
+</footer>
+
+<style>
+  footer {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: rebeccapurple;
+    min-height: 3rem;
+  }
+</style>
