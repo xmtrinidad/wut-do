0b47a5e73a15cb9fa57e0335ca0ab34092dc68a1
Matt <nevermindthelabel@gmail.com>
2022-02-11 21:36:52 -0700

    added some style

--- client/src/components/app-header.svelte
+++ client/src/components/app-header.svelte
@@ -13,7 +13,7 @@
     <span class="app-title-or-whatever">WUT DO - NOT AN AWS APP</span>
   </div>
   <div class="header-right">
-    <ul>
+    <ul class="pages">
       {#each theRoutes as aRoute}
         <!-- Svelte magic. If isActive is true, the "active" class is applied. -->
         <li class:active={$isActive(aRoute.path)}>
@@ -46,5 +46,12 @@
   }
   .active {
     font-weight: bold;
+  }
+  .pages {
+    display: flex;
+    margin: 0px 1em 0 auto;
+  }
+  .pages li {
+    margin: 0 0.5em;
   }
 </style>
