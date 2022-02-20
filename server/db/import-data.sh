b62e3ff53def0bf80dd99de5639c04edd6dd02f6
Matt <nevermindthelabel@gmail.com>
2022-02-19 21:11:02 -0700

    updating db password

--- server/db/import-data.sh
+++ server/db/import-data.sh
@@ -2,7 +2,7 @@
 sleep 90s
 
 #run the setup script to create the DB and the schema in the DB
-/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P "$ecurePa$$word1!" -i setup.sql
+/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P "superSecurePasswordBrah" -i setup.sql
 
 #import the data from the csv file
 # /opt/mssql-tools/bin/bcp heroes.dbo.HeroValue in "/usr/work/heroes.csv" -c -t',' -S localhost -U SA -P "Password1!" -d heroes
