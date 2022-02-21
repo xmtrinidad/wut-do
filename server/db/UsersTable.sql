a1536eb74f90ea66f2dc6128a4cb51df27d0cbce
Matt <nevermindthelabel@gmail.com>
2022-02-20 20:09:39 -0700

    added sql files

---
+++ server/db/UsersTable.sql
@@ -0,0 +1,22 @@
+USE [WutDoDB]
+GO
+
+/****** Object:  Table [dbo].[users]    Script Date: 2/20/2022 8:08:46 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[users](
+	[userId] [int] IDENTITY(1,1) NOT NULL,
+	[username] [varchar](50) NOT NULL,
+	[password] [varchar](50) NOT NULL,
+ CONSTRAINT [PK_users] PRIMARY KEY CLUSTERED 
+(
+	[userId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+
--- server/db/setup.sql
+++
@@ -1,6 +0,0 @@
-CREATE DATABASE WutDoUsers;
-GO
-USE WutDoUsers;
-GO
-CREATE TABLE users (id INT, username VARCHAR(50), password VARCHAR(50));
-GO
