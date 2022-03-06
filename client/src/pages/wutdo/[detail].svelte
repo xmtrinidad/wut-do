4e7719f53a9edb028ff4de7efa0a4cab5e368684
Xavier <xmtrinidad@gmail.com>
2022-03-06 14:33:55 -0700

    added list

--- client/src/pages/wutdo/[detail].svelte
+++ client/src/pages/wutdo/[detail].svelte
@@ -31,6 +31,15 @@
     animation: glow 1s ease-in-out infinite alternate;
   }
 
+  ul {
+    list-style: circle;
+  }
+
+  li {
+    padding: 8px;
+    font-size: 1.2rem;
+  }
+
     @keyframes glow {
     from {
       text-shadow: 0 0 10px #fff, 0 0 20px #fff, 0 0 30px #e60073, 0 0 40px #e60073, 0 0 50px #e60073, 0 0 60px #e60073, 0 0 70px #e60073;
